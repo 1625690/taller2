a7bdadbd336bf046e898f641674eac69101fd123
invitado <invitado@s4pc7.eisc.univalle.edu.co>
2017-03-29 10:37:35 -0500

    Clase cliente y representante comentadas

--- src/Modelo/Representante.java
+++ src/Modelo/Representante.java
@@ -6,9 +6,32 @@
 package Modelo;
 
 /**
- *
+ * Clase del representante legal de una empresa
+ * Hereda los atributos y metodos de cliente
  * @author invitado
  */
-public class Representante {
+public class Representante extends Cliente{
+
+    //-------------------------------------------------------------------------
+    // ATR
+    //-------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
+    // CONSTRUCTOR
+    //-------------------------------------------------------------------------
     
+    /**
+     * Constructor de Representante
+     * @param nom - Nombre del cliente
+     * @param ape - Apellido del cliente
+     * @param ced - Cedula del cliente
+     * @param edad -  Edad del cliente
+     * @param genero - Genero del cliente
+     * @param in - Ingresos del cliente
+     * @param eg - Egresos del cliente
+     * @param act - Actividad económica
+     */
+    public Representante(String nom, String ape, String ced, String edad, String genero, String in, String eg, String act) {
+        super(nom, ape, ced, edad, genero, in, eg, act);
+    }
+ 
 }
